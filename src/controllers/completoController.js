529f27f86ea553fa4a16d37032636d0ea7f16229
Pittheone <pedroascui.fenner@gmail.com>
2025-05-30 16:03:39 -0400

    controladores listos

---
+++ src/controllers/completoController.js
@@ -0,0 +1,26 @@
+const mongoose = require('mongoose');
+const bcrypt = require('bcryptjs');
+
+const completoSchema = mongoose.Schema({
+    name: {
+        type: String,
+        required: true,
+    },
+    price: {
+        type: String,
+        required: true,
+    },
+    shippingAdress: {
+        type: String,
+        required: true,
+    },
+    customization: {
+        type: String
+    }
+}, {
+    timestamps: true,
+})
+
+const Completo = mongoose.model('Completo', completoSchema);
+
+module.exports = Completo;
