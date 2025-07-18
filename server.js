533c93720ac35b5dd1ce75a100851a687007641b
Pittheone <pedroascui.fenner@gmail.com>
2025-07-18 19:46:36 -0400

    server updated!

--- server.js
+++ server.js
@@ -1,25 +1,21 @@
-const connectDB = require('./src/config/db');
 const express = require('express');
+const app = express();
 const cors = require('cors')
-const dotenv = require('dotenv');
-// require('dotenv').config();
-
-//const mongoose = require('mongoose');
 const cookieParser = require('cookie-parser');
-//donde va dotenv.config()?
-dotenv.config();
+const connectDB = require('./src/config/db');
+require('dotenv').config();
 const userRoutes = require('./src/routes/userRoutes');
 const completoRoutes = require('./src/routes/completoRoutes');
 const cartRouter = require('./src/routes/cart.routes');
 
-
+const PORT = process.env.PORT || 5000;
 connectDB();
 
-const app = express();
 const isProd = process.env.NODE_ENV === 'production';
 const allowedOrigins = isProd 
 ? process.env.FRONTEND_URL
 : process.env.FRONTEND_URL_DEV;
+
 app.use(
   cors({
     origin: allowedOrigins,
@@ -32,10 +28,6 @@ app.use(cookieParser());
 app.use('/api/users', userRoutes);
 app.use('/api/completo', completoRoutes);
 app.use('/api/carts', cartRouter);
-
-
-
-const PORT = process.env.PORT || 5000;
 
 app.listen(PORT, () => {
   console.log("server running on port: ", PORT);
